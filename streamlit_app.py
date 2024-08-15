c7a9f1d5ba7c53bde93fab01f15042367c937939
Shreeganesha35 <smita.desai@dypvp.edu.in>
2024-08-15 17:51:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ except Exception as e:
 # Define categories and thresholds
 categories = ["Black Rot", "ESCA", "Healthy", "Leaf Blight", "Healthy_Pomogranate", "Cercospora", "Bacterial_Blight", "Anthracnose"]
 thresholds = {
-    "Healthy_Pomogranate": 0.6,  # Set your threshold for Healthy_Pomogranate
-    "Cercospora": 0.05            # Set your threshold for Cercospora
+    "Healthy_Pomogranate": 1.0,  # Set your threshold for Healthy_Pomogranate
+    "Cercospora": 1.0            # Set your threshold for Cercospora
 }
 
 # Apply custom CSS for background and prediction box styling
